6aa6facdb57d325fde36372055eed8d55083861c
Hugh Mungus <Hugh Mungus>
2016-09-19 15:33:29 -0400

    Added temperature/humidity sensor that outputs on 16x2 LCD from previous LCD project

---
+++ Temp_LCD/SHT15 Lib/SHT15_Breakout-master/README.md
@@ -0,0 +1,32 @@
+SHT15 Breakout
+==============
+[![SHT15 Breakout](https://cdn.sparkfun.com/assets/parts/1/1/1/3/9/13683-01.jpg)  
+*SHT15 Breakout Board (SEN-13683)*](https://www.sparkfun.com/products/13683)
+
+Repository Contents
+-------------------
+* **/Hardware** - All Eagle design files (.brd, .sch)
+* **/Libraries** - Contains the Arduino library and examples for the sensor
+* **/Production** - Test bed files and production panel files
+
+
+Documentation
+--------------
+* **[Hookup Guide](https://learn.sparkfun.com/tutorials/sht15-humidity-and-temperature-sensor-hookup-guide)** - Basic hookup guide for the sensor.
+* **[SparkFun Fritzing repo](https://github.com/sparkfun/Fritzing_Parts)** - Fritzing diagrams for SparkFun products.
+* **[SparkFun 3D Model repo](https://github.com/sparkfun/3D_Models)** - 3D models of SparkFun products. 
+
+License Information
+-------------------
+This product is _**open source**_! 
+
+The **hardware** is released under [Creative Commons ShareAlike 4.0 International](https://creativecommons.org/licenses/by-sa/4.0/).
+
+The **code** is beerware; if you see me (or any other SparkFun employee) at the local, and you've found our code helpful, please buy us a round!
+
+Please use, reuse, and modify these files as you see fit. Please maintain attribution to SparkFun Electronics and release anything derivative under the same license.
+
+Distributed as-is; no warranty is given.
+
+- Your friends at SparkFun.
+
---
+++ Temp_LCD/Temp_LCD.ino
@@ -0,0 +1,81 @@
+/******************************************************************
+SHT15 Example + LCD modification
+
+Connections:
+GND  -> A2
+Vcc  -> A3
+DATA -> A4
+SCK  -> A5
+
+******************************************************************************/
+#include <SHT1X.h> //STH15 lib
+#include <LiquidCrystal.h> // LCD lib
+//variables for storing values
+LiquidCrystal lcd(4,5,6,7,8,9); // LCD pins in use
+
+float tempC = 0;
+float tempF = 0;
+float humidity = 0;
+
+//Create an instance of the SHT1X sensor
+SHT1x sht15(A4, A5);//Data, SCK
+
+//delacre output pins for powering the sensor
+int power = A3;
+int gnd = A2;
+
+void setup()
+{
+  Serial.begin(9600); // Open serial connection to report values to host
+
+  lcd.begin (16, 2); // LCD column by row size
+  lcd.clear(); // Clears anything on LCD
+  
+  pinMode(power, OUTPUT);
+  pinMode(gnd, OUTPUT);
+
+  digitalWrite(power, HIGH);
+  digitalWrite(gnd, LOW);
+}
+//-------------------------------------------------------------------------------------------
+void loop()
+{
+  readSensor();
+  printOut();
+  delay(1000);
+  
+  lcd.clear();
+  lcd.setCursor(0,0);
+
+    lcd.print("Temp= ");
+    lcd.print(tempF);
+    lcd.print("F");
+
+  lcd.setCursor(0,1);
+
+    lcd.print("Humidity= ");
+    lcd.print(humidity);
+    lcd.print("%");
+    
+  
+}
+//-------------------------------------------------------------------------------------------
+void readSensor()
+{
+  // Read values from the sensor
+  tempC = sht15.readTemperatureC();
+  tempF = sht15.readTemperatureF();
+  humidity = sht15.readHumidity();  
+}
+//-------------------------------------------------------------------------------------------
+void printOut()
+{
+  Serial.print(" Temp = ");
+  Serial.print(tempF);
+  Serial.print("F, ");
+  Serial.print(tempC);
+  Serial.println("C");
+  Serial.print(" Humidity = ");
+  Serial.print(humidity); 
+  Serial.println("%");
+}
